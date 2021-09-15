ec44cd2c4670ff770d0df7dabe638f8f05d6df80
Ben Joseph <benjosephcr7@gmail.com>
2021-09-16 00:58:17 +0530

    ddd

--- js/customscript.js
+++ js/customscript.js
@@ -13,6 +13,7 @@ if((category!="" && category!=null) && (subCategory!="" && subCategory!=null)){
    url = "http://www.mychurchmanageronline.com/api/data/"+category+"/"+subCategory;
 }
 else if(search!="" && search!=null){
+  $("#main-search").val(search);
   url = "http://www.mychurchmanageronline.com/api/data/search/"+search;
 }
 var type = "GET";
@@ -24,7 +25,7 @@ var handleData = function (response) {
         var msg="Order Confirmed";
      
       console.log(obj.prodname,obj.price);
-      $('#divMainAppend').append('<div class="col-lg-4 col-md-6 col-sm-6"> <div class="product-m"> <div class="product-m__thumb"> <a class="aspect aspect--bg-grey aspect--square u-d-block" href="product-detail.html"> <img class="aspect__img" src="images/product/men/product6.jpg" alt=""></a> <div class="product-m__quick-look"> <a class="fas fa-search" data-modal="modal" data-modal-id="#quick-look" data-tooltip="tooltip" data-placement="top" title="Quick Look"></a></div> <div class="product-m__add-cart"> <a class="btn--e-brand" data-modal="modal" data-modal-id="#add-to-cart">Add to Cart</a></div> </div> <div class="product-m__content"> <div class="product-m__name"> <a href="product-detail.html">'+obj.product_name+'</a> <a href="https://api.whatsapp.com/send?phone=919778123839&text=I%27m%20api%20msg%20hello%20'+name+'%20friend%20'+msg+'" target="_blank"> <input type="image" src="https://i.ibb.co/N9P0K9H/239px-Whats-App-svg.png" alt="Submit" width="40" height="40"> </a> </div> <div class="product-m__price">Rs.'+obj.price+'</div> <div class="product-m__hover"> <div class="product-m__preview-description"> <span>'+obj.description+'</span></div></div> </div> </div> </div>');
+      $('#divMainAppend').append('<div class="col-lg-4 col-md-6 col-sm-6"> <div class="product-m"> <div class="product-m__thumb"> <a class="aspect aspect--bg-grey aspect--square u-d-block"> <img class="aspect__img" src="images/product/men/product6.jpg" alt=""></a> <div class="product-m__quick-look"> <a class="fas fa-search" data-modal="modal" data-modal-id="#quick-look" data-tooltip="tooltip" data-placement="top" title="Quick Look"></a></div> <div class="product-m__add-cart"> <a class="btn--e-brand" data-modal="modal" data-modal-id="#add-to-cart">Add to Cart</a></div> </div> <div class="product-m__content"> <div class="product-m__name"> <a>'+obj.product_name+'</a> <a href="https://api.whatsapp.com/send?phone=919778123839&text=I%27m%20api%20msg%20hello%20'+name+'%20friend%20'+msg+'" target="_blank"> <input type="image" src="https://i.ibb.co/N9P0K9H/239px-Whats-App-svg.png" alt="Submit" width="40" height="40"> </a> </div> <div class="product-m__price">Rs.'+obj.price+'</div> <div class="product-m__hover"> <div class="product-m__preview-description"> <span>'+obj.description+'</span></div></div> </div> </div> </div>');
 
     }
   }
--- js/indexpagescript.js
+++ js/indexpagescript.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 
 $("#btnMainSearch").click(function(){
     var searchQuery=$("#main-search").val();
-    alert(searchQuery);
 window.location.href="shop-side-version-2.html?search="+searchQuery;
 return false;
 });
