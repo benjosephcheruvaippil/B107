88814552f08c0ac9700c5569b8cd0dcee2e3137a
Ben Joseph <benjosephcr7@gmail.com>
2021-09-16 00:24:15 +0530

    Update indexpagescript.js

--- js/indexpagescript.js
+++ js/indexpagescript.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 
 $("#btnMainSearch").click(function(){
-alert("clicked");
-window.location.href="http://b107.mychurchmanageronline.com/shop-side-version-2.html?cat=football&sub=otheraccessories";
+    var searchQuery=$("#main-search").val();
+    alert(searchQuery);
+window.location.href="shop-side-version-2.html?cat=football&sub=otheraccessories";
 //window.location.replace("https://www.google.com");
-//return false;
+return false;
 });
 
 });
